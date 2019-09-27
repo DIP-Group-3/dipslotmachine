3ed6d32a9a38d31dd642370b9d8e51fa6106e28c
ThskXjy <thskxjy@gmail.com>
2019-09-27 23:48:42 +0800

    Integrate LCD & IR Sensor @ 23:48H

--- src/LcdUpdate/LcdUpdate.ino
+++ src/LcdUpdate/LcdUpdate.ino
@@ -25,31 +25,93 @@ void setup()
   servoMain.attach(MotorPin); 
   lcd.init();                      // initialize the lcd 
   pinMode(isObstaclePin, INPUT);
+  LcdMessage(0);
 }
 
 void loop()
 { 
   lcd.backlight();
-  lcd.setCursor(1,1);
   isObstacle = digitalRead(isObstaclePin);
-   
-  if(isObstacle == LOW) {             // Obstacle detected
+  if(isObstacle == LOW) {                       // Obstacle detected
     Serial.println("On");
     coinInsert = true;
-  } else {                            // No Obstacle
+  } else if(isObstacle == HIGH && coinInsert){  // No Obstacle AND coinInsert == True
     Serial.println("Off");
-    if(coinInsert){                   
-      creditAmt++;                    // Incremeent Credit Amt
-    }
-    coinInsert = false;               //
-  }
+    updateCredit(1);                            // Incremeent Credit Amt
+    LcdMessage(1);
+  }                   
+}
 
-  switch(creditAmt){
-    case 0 :  lcd.print("INSERT");
-              lcd.setCursor(1,2);  
+void updateCredit(int addAmt){       //Method: Update Credit Amount
+  if(addAmt>0 && creditAmt == 0){    //Coin inserted when current credit == 0
+    betAmt = 1;
+    creditAmt += addAmt;
+    coinInsert = !coinInsert;  
+  }else if(addAmt>0){                //Coin inserted
+    creditAmt += amt;
+    coinInsert = !coinInsert;  
+  }else{                             //Credit deducted when played
+    creditAmt += amt;               
+    if(creditAmt <= 0){              //When Credit less than or equal to 0
+      LcdMessage(0);
+    }else{                           //When credit greater than 0; 
+      LcdMessage(1);
+    }
+  }
+}
+
+void updateBet(){           //Method: Update Bet Amount when Bet Button Pressed
+  if(betAmt>=3){            //When current bet amount equal to/greater than 3
+    betAmt = 1;
+  }else{                    //When current bet amount is less than 3
+     betAmt++;
+  }
+}
+
+void LcdMessage(int scenario){
+  lcd.clear();                          //Clear previous displayed frame
+  switch(scenario){
+    //Idle State || Credit 0 Message Frame
+    case 0 :  lcd.setCursor(7,1);
+              lcd.print("INSERT");
+              lcd.setCursor(8,2);  
               lcd.print("COIN");
               break;
-    default:  lcd.print("Credit : ");
+
+    //Credit!=0 && 0<Bet<=3 Message Frame
+    case 1:   lcd.setCursor(1,1);
+              lcd.print("Credit : ");
+              lcd.print(creditAmt);
+              lcd.setCursor(1,2);   
+              lcd.print("Bet    : ");
+              lcd.print(betAmt);
+              break;
+    
+    //Lose Condition Message Frame
+    case 2:   lcd.setCursor(6,1);
+              lcd.print("NICE TRY");
+              lcd.setCursor(9,2);
+              lcd.print(":)");
+              break;
+
+    //Win Condition Message Frame
+    case 3:   lcd.setCursor(6,1);
+              lcd.print("YOU WON!");
+              lcd.setCursor(9,2);
+              lcd.print("$");
+              lcd.print(betAmt*2);
+              break;
+
+    //Jackpot Condition Message Frame
+    case 4:   lcd.setCursor(6,1);
+              lcd.print("JACKPOT!");
+              lcd.setCursor(9,2);
+              lcd.print(":)");
+              break;
+
+    //Default Message Frame
+    default:  lcd.setCursor(1,1);  
+              lcd.print("Credit : ");
               lcd.print(creditAmt);
               lcd.setCursor(1,2);   
               lcd.print("Bet : ");
