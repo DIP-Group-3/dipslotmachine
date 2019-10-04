77db04a8962142be70ece556c66be79d645341f4
ThskXjy <thskxjy@gmail.com>
2019-10-04 10:13:34 +0800

    Tidy Code

--- src/LcdUpdate/LcdUpdate.ino
+++ src/LcdUpdate/LcdUpdate.ino
@@ -1,39 +1,37 @@
-//DFRobot.com
-//Compatible with the Arduino IDE 1.0
-//Library version:1.1
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
 #include <Servo.h>
 
-//LCD Display: Declared Variable
+//LCD Display Variable
 int isObstaclePin = 7;
 int isObstacle = HIGH;
+LiquidCrystal_I2C lcd(0x27,20,4);                // Set the LCD address to 0x27 for a 20 chars and 4 line display
 
-//Buttons Declared Variable
+//Bet Button Variables
 const int betBtn = 12;
 int betState = LOW;
 bool betPressed = false;
 
+//Spin Button Variables
 const int spinBtn = 13;
 int spinState = LOW;
 bool spinPressed = false;
 
-//Server Motor Variable
-#define MotorPin 5
-
-Servo servoMain;
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x3F for a 20 chars and 4 line display
-
+//Credit, Bet variable
 int creditAmt =0, betAmt=0;
-long int winCount = 0;
+
+//IR sensor variable
 bool coinInsert = false;
 
+//Server Motor Variable
+#define MotorPin 5
+Servo servoMain;
 
 void setup()
 {
   Serial.begin(9600);
   servoMain.attach(MotorPin); 
-  lcd.init();                      // Initialize the lcd 
+  lcd.init();                                    // Initialize the LCD 
   pinMode(isObstaclePin, INPUT);
   pinMode(betBtn, INPUT);
   pinMode(spinBtn, INPUT);
@@ -44,46 +42,43 @@ void loop()
 { 
   lcd.backlight();
   isObstacle = digitalRead(isObstaclePin);
-  if(isObstacle == LOW) {                       // Obstacle detected
-    //Serial.println("On");
+  if(isObstacle == LOW) {                       // Obstacle detected 
     coinInsert = true;
   } else if(isObstacle == HIGH && coinInsert){  // No Obstacle AND coinInsert == True
-    //Serial.println("Off");
     updateCredit(1);                            // Incremeent Credit Amt
-    LcdMessage(1);
+    LcdMessage(1);                              // Display Message in LCD
   }
-
+  
   if(betAvail()){                               //Check if bet increment could be done
-    updateBet();
+    updateBet();                                    //Call method to update bet amt
   }else if(spinAvail()){                        //Ceck if spin could be done
-    //Spin Frames
-   activateSpin();
+    activateSpin();                                 //Call Method to start LED Matrix animation
   }
 }
 
-void updateCredit(int addAmt){       //Method: Update Credit Amount
-  if(addAmt>0 && creditAmt == 0){    //Scenario 1: Coin inserted when current credit == 0
+void updateCredit(int addAmt){                  //Method: Update Credit Amount
+  if(addAmt>0 && creditAmt == 0){               //Scenario 1: Coin inserted when current credit == 0
     betAmt = 1;
     creditAmt += addAmt;
     coinInsert = !coinInsert;  
-  }else if(addAmt>0){                //Scenario 2: Coin inserted when current credit !=0
+  }else if(addAmt>0){                           //Scenario 2: Coin inserted when current credit !=0
     creditAmt += addAmt;
     coinInsert = !coinInsert;  
-  }else{                             //Scenario 3: Credit deducted when played
+  }else{                                        //Scenario 3: Credit deducted when played
     creditAmt += addAmt;               
-    if(creditAmt <= 0){                   //Message Type 1: Credit less than or equal to 0
+    if(creditAmt <= 0){                             //Message Type 1: Credit less than or equal to 0
       LcdMessage(0);
-    }else{                                //Message Type 2: When credit greater than 0
+    }else{                                          //Message Type 2: When credit greater than 0
       LcdMessage(1);
     }
   }
 }
-void updateBet(){                   //Method: Update Bet Amount when Bet Button Pressed
-  if(betAmt>=3 || betAmt >= creditAmt){                    //Scenario 1: Current bet amount equal to/greater than 3
+void updateBet(){                               //Method: Update Bet Amount when Bet Button Pressed
+  if(betAmt>=3 || betAmt >= creditAmt){             //Scenario 1: Current bet amount equal to/greater than 3
     Serial.println("Bet = 1");
     Serial.println(betAmt);
     betAmt = 1;
-  }else{                            //Scenario 2: Current bet amount less than 3
+  }else{                                            //Scenario 2: Current bet amount less than 3
     Serial.println("Increase Bet");
     Serial.println(betAmt);
     betAmt++;
@@ -104,22 +99,23 @@ bool betAvail(){
     betPressed = false;
     return false;
   }
-  
 }
 
-void activateSpin(){
-  spinPressed = false;
-  creditAmt -= betAmt;
-  betAmt = 1;
+void activateSpin(){                                            //Method: Activate LED Matrix
+  spinPressed = false;                                              
+  creditAmt -= betAmt;                                              //Deducted credit based on bet amount
   if(creditAmt <=0){
-    LcdMessage(0);
+    creditAmt = 0;                                                  //Credit amount set back to 0
+    betAmt = 0;                                                     //Bet amount set back to 0
+    LcdMessage(0);                                                  //Display "Insert Coin" Message when credit less than 0    
   }else{
-    LcdMessage(1);
+    betAmt = 1;                                                     //Bet amount set to 1
+    LcdMessage(1);                                                  //Display remaining credit and bet amounts in LCD
   }
 }
 
 bool spinAvail(){
-  spinState = digitalRead(spinBtn);        //Read  Spin Btn Input
+  spinState = digitalRead(spinBtn);                               //Read  Spin Btn Input
   if(spinState == HIGH){
     spinPressed = true;
   }else if (spinState == LOW && creditAmt > 0 && spinPressed){
@@ -131,11 +127,10 @@ bool spinAvail(){
   }
 }
 
-
-
 void LcdMessage(int scenario){
-  lcd.clear();                          //Clear current displayed frame
+  lcd.clear();                                                    //Clear current displayed frame
   switch(scenario){
+    
     //Idle State || Credit 0 Message Frame
     case 0 :  lcd.setCursor(7,1);
               lcd.print("INSERT");
