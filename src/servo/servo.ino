b4871425cd0bfad17aa30295f7f0b5c85e0128cd
lmorocks <NHENG003@ntu.edu.sg>
2019-10-04 11:17:56 +0800

    cleaned servo code

--- src/servo/servo.ino
+++ src/servo/servo.ino
@@ -1,25 +1,24 @@
 #include <Servo.h>
+
+//global variables
 Servo servoMain;
-int led = 13;
-long int winCount = 0;
+int servoEndAngle = 20;
+int servoStartAngle = 90;
 
 void setup() {         
   Serial.begin(9600);
   pinMode(led, OUTPUT);
   servoMain.attach(5); // servo on digital pin 5
+  servoMain.write(startAngle); //initialise servo position
 }
-
-int minAngle = 20;
-int maxAngle = 90;
-
 
 void dispenseCoins(int numOfCoins) {
   for (int i = 0; i < numOfCoins; i++) {
-    servoMain.write(maxAngle);
+    servoMain.write(servoStartAngle);
     delay(200);
-    servoMain.write(minAngle);
+    servoMain.write(servoEndAngle);
     delay(200);
-    servoMain.write(maxAngle);
+    servoMain.write(servoStartAngle);
     delay(200);
   }
 }
